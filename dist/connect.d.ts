b2930704329092c666172341fcbb0eb9ced0217f
masiulis <karolis.masiulis@ugnis.com>
2020-05-18 13:10:08 +0300

    Cleanup

--- dist/connect.d.ts
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export declare const connect: (Component: React.ComponentType) => React.ComponentType;
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Emitter } from "./eventEmitter";
-export { connect } from "./connect";
-export { proxify as lape } from "./proxify";
-export { useLape } from "./hook";
