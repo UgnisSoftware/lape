173830d00a7758316abdb645a8e1451ce28f3f69
masiulis <karolis.masiulis@ugnis.com>
2020-10-15 10:42:35 +0300

    Expose testing types

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { connect } from "./connect/connect";
 export { proxify as lape } from "./proxify";
 export { useLape } from "./hook/hook";
 export * from "./undo/undo";
+export * from "./testing";
