873e3fa35143c689c816f28bc086a1199c62dc7f
masiulis <karolis.masiulis@ugnis.com>
2020-10-22 18:09:13 +0300

    Expose useEffectLape

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as Emitter } from "./Emitter";
 export { connect } from "./connect/connect";
 export { proxify as lape } from "./proxify";
-export { useLape } from "./hook/hook";
+export { useLape, useEffectLape } from "./hook/hook";
 export * from "./undo/undo";
 export * from "./testing";
