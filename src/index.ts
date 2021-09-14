3e3d61e44ca00794ec5a46e1db58c370917687ae
masiulis <karolis.masiulis@ugnis.com>
2021-09-14 22:52:16 +0300

    expose JSX

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export { connect } from "./connect/connect";
 export { proxify as lape, ignoreState } from "./proxify";
 export { useLape, useLapeEffect } from "./hook/hook";
 export { lapeTrackUseState } from "./hacks/lapeTrackUseState";
+export { jsx as _lapeJSX } from "./jsx-runtime";
 export * from "./undo/undo";
 export * from "./testing";
