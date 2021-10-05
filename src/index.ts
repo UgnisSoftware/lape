7e2c15dacbcf362dfac2d8c6e51fe7c5d2976353
masiulis <karolis.masiulis@ugnis.com>
2021-10-05 12:52:46 +0300

    Add createElement to jsx-runtime

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { connect } from "./connect/connect";
 export { proxify as lape, ignoreState } from "./proxify";
 export { useLape, useLapeEffect } from "./hook/hook";
 export { lapeTrackUseState } from "./hacks/lapeTrackUseState";
-export { jsxOld as _lapeViteJSX } from "./jsx-runtime";
+export { jsxOld as _lapeViteJSX, createElement } from "./jsx-runtime";
 export * from "./undo/undo";
 export * from "./testing";
